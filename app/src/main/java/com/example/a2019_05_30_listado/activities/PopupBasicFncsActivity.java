c4dd8c84c7d4fa88ecf14a0e69a991840a5293ad
sergio <www3.splcoder@gmail.com>
2019-06-06 10:43:00 +0200

    Some changes

--- app/src/main/java/com/example/a2019_05_30_listado/activities/PopupBasicFncsActivity.java
+++ app/src/main/java/com/example/a2019_05_30_listado/activities/PopupBasicFncsActivity.java
@@ -42,20 +42,13 @@ public class PopupBasicFncsActivity extends AppCompatActivity implements View.On
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_popup_basic_fncs);
 
-		DisplayMetrics dm = new DisplayMetrics();
+		/*DisplayMetrics dm = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics( dm );
 
 		int width	= (int)(dm.widthPixels * 0.8);
 		int height	= (int)(dm.heightPixels * 0.7);
 
-		getWindow().setLayout( width, height );
-
-		/*WindowManager.LayoutParams params = getWindow().getAttributes();
-		params.gravity = Gravity.CENTER;
-		params.x = 0;
-		params.y = -20;
-
-		getWindow().setAttributes( params );*/
+		getWindow().setLayout( width, height );*/
 
 		//--------------------------
 
