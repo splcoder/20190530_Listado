9c1d78a0dfce2d8ad6f1e55d01ca5622b35d597e
sergio <www3.splcoder@gmail.com>
2019-06-25 10:33:24 +0200

    Variable class modified

--- app/src/main/java/com/example/a2019_05_30_listado/data/fncs/base/Variable.java
+++ app/src/main/java/com/example/a2019_05_30_listado/data/fncs/base/Variable.java
@@ -1,16 +1,59 @@
 package com.example.a2019_05_30_listado.data.fncs.base;
 
+import static com.example.a2019_05_30_listado.data.fncs.base.Constant.NO_CONSTANT;
+
 public class Variable extends FncBase {
 
-	// TODO
+	private double value = 0;
+	private String sValue = "0";
+	private Constant constant = NO_CONSTANT;	// For showing: Pi, e, ..., instead of its value
 
-	@Override
-	public double output() {
-		return 0;
+	private String name = "X";
+	public void setName( String name ){
+		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "";
+	private Variable( FncBase parent ){
+		this.parent = parent;
+		this.type = FncBase.VALUE;
+		setNoArgument();
 	}
+
+	public Variable( FncBase parent, double value ){
+		this( parent );
+		this.value = value;
+		this.sValue = String.valueOf( value );
+	}
+	public Variable( FncBase parent, double value, String name ){
+		this( parent, value );
+		this.name = name;
+	}
+
+	public Variable( FncBase parent, String number ){
+		this( parent );
+		this.value = Double.parseDouble( number );
+		this.sValue = number;
+	}
+	public Variable( FncBase parent, String number, String name ){
+		this( parent, number );
+		this.name = name;
+	}
+
+	public Variable( FncBase parent, Constant constant ){
+		this( parent );
+		this.constant = constant;
+		this.value = Constant.get( constant );
+		this.sValue = Constant.toString( constant );
+	}
+	public Variable( FncBase parent, Constant constant, String name ){
+		this( parent, constant );
+		this.name = name;
+	}
+
+	public boolean isConstant(){ return constant != NO_CONSTANT; }
+
+	@Override
+	public double output(){ return value; }
+	@Override
+	public String toString(){ return name; }
 }
